8574bd3787e6d7e9380338c69a5e9340f83bf3bd
Feterson1 <Feterson@bk.ru>
2023-01-16 01:27:49 +0600

    pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,56 @@
 import React from 'react';
-import style from './Users.module.css';
+import styles from './Users.module.css';
 import axios from "axios";
 import userPhoto from "../../assets/images/user.png"
 
 class Users extends React.Component{
-        constructor(props){
-            super(props);
-            alert('Создался обьект');
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        
-            this.props.setUsers(response.data.items);
-       });
-        }
-
-
-
      
+     componentDidMount(){
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+            });
+            
+     }
+
+     onPageChanged(pageNumber){
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+            });
+     }
 
     render(){
+        
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for(let i = 1;i<=pagesCount;i++){
+           
+            pages.push(i);}
+
+            let curP = this.props.currentPage;
+            let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+            let curPL = curP + 5;
+            let slicedPages = pages.slice( curPF, curPL);
         
         return(
 
     <div>
+        <div>
+        {slicedPages.map( p => {
+            return <span className={this.props.currentPage === p && styles.selectedPage }
+            onClick = {(e) =>{this.onPageChanged(p)}}>{p}</span>
+           })}
+        </div>
 
 
         {this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.userPhoto} alt="" />
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="" />
                 </div>
                 <div>
 
